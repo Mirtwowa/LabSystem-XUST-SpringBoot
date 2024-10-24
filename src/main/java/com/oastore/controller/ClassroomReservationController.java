13cc96d9d46465b1d3460a2e283674320e2252f3
vioCleri <cjl150531@163.com>
2024-10-24 12:02:23 +0800

    预约修正

--- src/main/java/com/oastore/controller/ClassroomReservationController.java
+++ src/main/java/com/oastore/controller/ClassroomReservationController.java
@@ -14,12 +14,18 @@ public class ClassroomReservationController {
     @Autowired
     private ReservationService reservationService;
 
+    /**
+     * name: 预约人姓名
+     * startTime: 预约起始时间
+     * endTime : 预约结束时间
+     * */
     @PostMapping("/reserve")
     public String reserveClassroom(@RequestParam String name, @RequestParam String startTime, @RequestParam String endTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime start = LocalDateTime.parse(startTime, formatter);
         LocalDateTime end = LocalDateTime.parse(endTime, formatter);
         boolean success = reservationService.reserveClassroom(name, start,end);
+        System.out.println(success ? "预约成功" : "预约失败，该时间段已被预约");
         return success ? "预约成功" : "预约失败，该时间段已被预约";
     }
 }
